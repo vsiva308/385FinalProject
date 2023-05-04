4c2a5083d0a4da051d48982d314c1cc6f3a2311f
aclem02 <clemens9@illinois.edu>
2023-05-04 04:13:25 -0500

    collision & preliminary knockback

--- files/PlayerControl.sv
+++ files/PlayerControl.sv
@@ -3,18 +3,18 @@ module  PlayerControl ( input Reset, frame_clk,
                output [9:0]  Player1X, Player1Y, Player2X, Player2Y);
 					
 					
-    logic [9:0] Player1_X_Pos, Player1_Y_Pos;
-	 logic [9:0] Player1_X_Motion, Player1_Y_Motion;
+    int Player1_X_Pos, Player1_Y_Pos;
+	 int Player1_X_Motion, Player1_Y_Motion;
 	 
-	 logic [9:0] Player2_X_Pos, Player2_Y_Pos;
-	 logic [9:0] Player2_X_Motion, Player2_Y_Motion;
+	 int Player2_X_Pos, Player2_Y_Pos;
+	 int Player2_X_Motion, Player2_Y_Motion;
 	 
     parameter [9:0] Player1_X_Center=40;  // Center position on the X axis
 	 parameter [9:0] Player2_X_Center=480;  // Center position on the X axis
-    parameter [9:0] Player1_Y_Center=220;  // Center position on the Y axis
-	 parameter [9:0] Player2_Y_Center=160;  // Topleft position on the Y axis
-    parameter [9:0] Bound_X_Min=4;       // Leftmost point on the X axis
-    parameter [9:0] Bound_X_Max=639;     // Rightmost point on the X axis
+    parameter [9:0] Player1_Y_Center=215;  // Center position on the Y axis
+	 parameter [9:0] Player2_Y_Center=170;  // Topleft position on the Y axis
+    parameter [9:0] Bound_X_Min=2;       // Leftmost point on the X axis
+    parameter [9:0] Bound_X_Max=637;     // Rightmost point on the X axis
     parameter [9:0] Bound_Y_Min=0;       // Topmost point on the Y axis
     parameter [9:0] Bound_Y_Max=479;     // Bottommost point on the Y axis
     parameter [9:0] Ball_X_Step=2;      // Step size on the X axis
@@ -36,15 +36,10 @@ module  PlayerControl ( input Reset, frame_clk,
 	 int Player2_top;
 	 int Player2_bottom;
 	 
-	 logic[9:0] overlap_left;
-	 logic[9:0] overlap_right;
-	 logic[9:0] overlap_top;
-	 logic[9:0] overlap_bottom;
+	 int XDist;
 	 
-	 int Player1_Collision_X;
-	 int Player1_Collision_Y;
-	 int Player2_Collision_X;
-	 int Player2_Collision_Y;
+	 int Player1_Knockback;
+	 int Player2_Knockback;
 	 
 	 logic[9:0] keycodeAllP1X, keycodeAllP1Y, keycodeAllP2X, keycodeAllP2Y;
 	 
@@ -58,6 +53,8 @@ module  PlayerControl ( input Reset, frame_clk,
 	 
 	 assign Player2X = Player2_X_Pos;
     assign Player2Y = Player2_Y_Pos;
+	 
+	 assign XDist = Player2_X_Pos - Player1_X_Pos;
 	
     always_ff @ (posedge Reset or posedge frame_clk )
     begin: Move_Player1
@@ -72,12 +69,12 @@ module  PlayerControl ( input Reset, frame_clk,
 				begin
 				Player1_X_Motion <= 0;
 				JumpP1 <= 0;
-				if (keycode_0 == 8'h04 || keycode_1 == 8'h04 || keycode_2 == 8'h04 || keycode_3 == 8'h04)
+				if ((keycode_0 == 8'h04 || keycode_1 == 8'h04 || keycode_2 == 8'h04 || keycode_3 == 8'h04) && (Player1_X_Pos > Bound_X_Min))
 					begin
 						Player1_X_Motion <= -2;//A
 						JumpP1 <= 0;
 					end
-				if (keycode_0 == 8'h07 || keycode_1 == 8'h07 || keycode_2 == 8'h07 || keycode_3 == 8'h07)
+				if ((keycode_0 == 8'h07 || keycode_1 == 8'h07 || keycode_2 == 8'h07 || keycode_3 == 8'h07) && (XDist > 105))
 					begin
 						Player1_X_Motion <= 2;//D
 						JumpP1 <= 0;
@@ -93,8 +90,8 @@ module  PlayerControl ( input Reset, frame_clk,
 					end
 
 			
-				Player1_Y_Pos <= (Player1_Y_Pos + Player1_Y_Motion + Player1_Collision_Y);  // Update ball position
-				Player1_X_Pos <= (Player1_X_Pos + Player1_X_Motion + Player1_Collision_X);	
+				Player1_Y_Pos <= (Player1_Y_Pos + Player1_Y_Motion);  // Update ball position
+				Player1_X_Pos <= (Player1_X_Pos + Player1_X_Motion + Player1_Knockback);	
 				end
 			end
 	 logic JumpP1 = 1'b0;
@@ -104,8 +101,7 @@ module  PlayerControl ( input Reset, frame_clk,
 		.clk(frame_clk),
 		.Jump(JumpP1),
 		.Ball_Y_Motion(Player1_Y_Motion)
-		);
-			
+		); 
 			
 	always_ff @ (posedge Reset or posedge frame_clk )
     begin: Move_Player2
@@ -121,12 +117,12 @@ module  PlayerControl ( input Reset, frame_clk,
 				begin
 				Player2_X_Motion <= 0;
 				JumpP2 = 0;
-				if (keycode_0 == 8'h0d || keycode_1 == 8'h0d || keycode_2 == 8'h0d || keycode_3 == 8'h0d)
+				if ((keycode_0 == 8'h0d || keycode_1 == 8'h0d || keycode_2 == 8'h0d || keycode_3 == 8'h0d) && (XDist > 105))
 					begin
 						Player2_X_Motion <= -2;//A
 						JumpP2 <= 0;
 					end
-				if (keycode_0 == 8'h0f || keycode_1 == 8'h0f || keycode_2 == 8'h0f || keycode_3 == 8'h0f)
+				if ((keycode_0 == 8'h0f || keycode_1 == 8'h0f || keycode_2 == 8'h0f || keycode_3 == 8'h0f) && ((Player2_X_Pos + 125) < Bound_X_Max))
 					begin
 						Player2_X_Motion <= 2;//D
 						JumpP2 <= 0;
@@ -141,8 +137,8 @@ module  PlayerControl ( input Reset, frame_clk,
 						JumpP2 <= 1; //W
 					end
 				 
-				Player2_Y_Pos <= (Player2_Y_Pos + Player2_Y_Motion + Player2_Collision_Y);  // Update ball position
-				Player2_X_Pos <= (Player2_X_Pos + Player2_X_Motion + Player2_Collision_X);
+				Player2_Y_Pos <= (Player2_Y_Pos + Player2_Y_Motion);  // Update ball position
+				Player2_X_Pos <= (Player2_X_Pos + Player2_X_Motion + Player2_Knockback);
 				end
 
 				
@@ -155,79 +151,39 @@ module  PlayerControl ( input Reset, frame_clk,
 		.Jump(JumpP2),
 		.Ball_Y_Motion(Player2_Y_Motion)
 		);
-	
-	 
-	 
-	 always_comb
-		begin
-			Player1_left = Player1_X_Pos;
-			Player1_right = Player1_X_Pos + Player1_Width;
-			Player1_top = Player1_Y_Pos; 
-			Player1_bottom = Player1_Y_Pos + Player1_Height;
-			
-			Player2_left = Player2_X_Pos; 
-			Player2_right = Player2_X_Pos + Player2_Width;
-			Player2_top = Player2_Y_Pos; 
-			Player2_bottom = Player2_Y_Pos + Player2_Height;
-			
-			//overlap_left = Player1_left - Player2_right; 
-			//overlap_right = Player2_left - Player1_right;
-			//overlap_top = Player1_top - Player2_bottom;
-			//overlap_bottom = Player2_top - Player1_bottom;
-		end
-	 
-	 	 
-	 always_ff @ (posedge Reset or posedge frame_clk)
-	 begin: Collision_Engine
-			if(Reset)
-			begin
-					Player1_Collision_X <= 0;
-					Player1_Collision_Y <= 0;
-					Player2_Collision_X <= 0;
-					Player2_Collision_Y <= 0;
-					//overlap_left <= 0;
-					//overlap_right <= 0;
-					//overlap_top <= 0;
-					//overlap_bottom <= 0;
-			end
-			else
-				begin 
-				if((Player1_right) >= Bound_X_Max)
-					begin
-						Player1_Collision_X <= Bound_X_Max - Player1_right;
-					end
-				else if((Player1_X_Pos) <= Bound_X_Min)
-					begin
-						Player1_Collision_X <= Bound_X_Min - Player1_X_Pos;
-					end
-				else
-					begin
-						Player1_Collision_X <= 0;
-						Player1_Collision_Y <= 0;
-					end
-					
-					
-				if((Player2_right) >= Bound_X_Max)
-					begin
-						Player2_Collision_X <= Bound_X_Max - Player2_right;
-					end
-				else if(Player2_X_Pos <= Bound_X_Min)
-					begin
-						Player2_Collision_X <= Bound_X_Min - Player2_X_Pos;
-					end
-				else
-					begin
-						Player2_Collision_X <= 0;
-						Player2_Collision_Y <= 0;
-					end
-				end
-				
-				//if((overlap_left != 0) && (Player1_left < Player2_right))
-					//begin
-						//Player1_Collision_X <= overlap_left;
-						//Player2_Collision_X <= overlap_right;
-					//end
-				
-	 end
 
-endmodule
+	 always_ff @ (posedge frame_clk)
+	 begin: Knockback_Engine 
+			PunchP1 = 1'b0;
+			PunchP2 = 1'b0;
+				
+			if((XDist < 120) && ((keycode_0 == 8'h06) || (keycode_1 == 8'h06) || (keycode_2 == 8'h06) || (keycode_3 == 8'h06)))
+				begin
+					PunchP2 <= 1'b1;
+				end
+			if((XDist < 120) && ((keycode_0 == 8'h11) || (keycode_1 == 8'h11) || (keycode_2 == 8'h11) || (keycode_3 == 8'h11)))
+				begin
+					PunchP1 <= 1'b1;
+				end				
+	 end
+	 
+	 logic PunchP1 = 1'b0;
+	 logic PunchP2 = 1'b0;
+	 
+	 KcontrolP1 KControlP1(
+		.Reset(Reset),
+		.clk(frame_clk),
+		.Punch(PunchP1),
+		.Ball_X_Motion(Player1_Knockback)
+		);
+	 
+	 KcontrolP2 KControlP2(
+		.Reset(Reset),
+		.clk(frame_clk),
+		.Punch(PunchP2),
+		.Ball_X_Motion(Player2_Knockback)
+		);
+	 
+	 
+	
+endmodule 
